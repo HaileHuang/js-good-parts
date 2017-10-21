f11eda5d42b95a1adf5a3ad5dc22173e4e31a814
黄攀 <huangpan@huangpans-MBP.lan>
2017-10-21 17:19:29 +0800

    Inheriatance with pseudoclassical

---
+++ PseudoClassical.js
@@ -0,0 +1,60 @@
+// We now have constructor functions that act like classes, but at the edges,
+// there may be surprising behavior. There is no privacy; all properties are public.
+// There is no access to super methods.
+
+var Mammal = function (name) {
+  this.name = name;
+}
+
+Mammal.prototype.getName = function () {
+  return this.name;
+}
+
+Mammal.prototype.says = function () {
+  return this.saying || '';
+}
+
+var myMammal = new Mammal('herb the Mammal');
+console.log(myMammal.getName());
+
+var Cat = function (name) {
+  this.name = name;
+  this.saying = 'meow';
+}
+
+Cat.prototype = new Mammal();
+Cat.prototype.constructor = Cat;
+Cat.prototype.purr = function (n) {
+  var i, s = '';
+  for (i = 0; i < n; i += 1) {
+    if (s) {
+      s += '-';
+    }
+    s += 'r';
+  }
+  return s;
+}
+
+var myCat = new Cat('Henrietta');
+console.log(myCat.says(), myCat.purr(5), myCat.getName());
+console.log(myCat);
+
+//Cascade Style
+var Cat2 = function (name) {
+  this.name = name;
+  this.saying = 'meow';
+}
+.inherits(Mammal)
+.method('purr', function (n) {
+  var i, s = '';
+  for (i = 0; i < n; i += 1) {
+    if (s) {
+      s += '-';
+    }
+    s += 'r';
+  }
+  return s;
+});
+
+var myCat2 = new Cat2('Henrietta');
+console.log(myCat2.says(), myCat2.purr(5), myCat2.getName());
--- public.js
+++ public.js
@@ -4,3 +4,28 @@ Function.prototype.method = function (name, func) {
   }
   return this;
 }
+
+Object.create = function (o) {
+  var F = function () {};
+  F.prototype = o;
+  return new F();
+}
+
+Object.method('superior', function (name) {
+  var that = this;
+  var method = that[name];
+  return function () {
+    return method.apply(that, arguments);
+  }
+})
+
+Function.method('new', function () {
+  var that = Object.create(this.prototype);
+  var other = this.apply(that, arguments);
+  return (typeof other === 'object' && other) || that;
+})
+
+Function.method('inherits', function (Parent) {
+  this.prototype = new Parent();
+  return this;
+})
